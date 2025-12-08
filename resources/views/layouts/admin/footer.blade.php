6385f3d9b1b981899af604aac7f0b1810d955443
Elvin Winata <elvin24si@mahasiswa.pcr.ac.id>
2025-12-08 10:06:57 +0700

    detail pengembang

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,9 +1,41 @@
-            <div class="container-fluid pt-4 px-4">
-                <div class="bg-light rounded-top p-4">
-                    <div class="row">
-                        <div class="col-12 text-center text-sm-start">
-                            &copy; <a href="#">Bina Desa Dokumen Hukum</a>, All Right Reserved.
-                        </div>
-                    </div>
+<div class="container-fluid pt-4 px-4">
+    <div class="bg-light rounded-top p-4">
+        <div class="row">
+            <div class="col-12 text-center text-sm-start">
+                &copy; <a href="#">Bina Desa Dokumen Hukum</a>, All Right Reserved.
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-12 text-center">
+
+                <img src="{{ asset('img/developer.jpg') }}"
+                     alt="Developer Photo"
+                     style="width: 70px; height: 70px; object-fit: cover;"
+                     class="rounded-circle mb-2">
+
+                <div><strong>Elvin Winata</strong></div>
+
+                <div class="text-muted" style="font-size: 14px;">
+                    NIM: 2457301039 • Prodi: Sistem Informasi
+                </div>
+
+                <div class="mt-2">
+
+                    <a href="https://linkedin.com/in/winataelvin" target="_blank" class="btn btn-sm btn-outline-primary">
+                        LinkedIn
+                    </a>
+
+                    <a href="https://github.com/elvin24si" target="_blank" class="btn btn-sm btn-outline-dark">
+                        GitHub
+                    </a>
+
+                    <a href="https://instagram.com/winataelvin" target="_blank" class="btn btn-sm btn-outline-danger">
+                        Instagram
+                    </a>
+
                 </div>
             </div>
+        </div>
+    </div>
+</div>
