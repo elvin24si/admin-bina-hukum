2dc470d546856d8f5838c59c592d5afd1e2effa8
Elvin Winata <elvin24si@mahasiswa.pcr.ac.id>
2025-12-15 10:03:12 +0700

    perbaikan header

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -1,5 +1,5 @@
     <meta charset="utf-8">
-    <title>BINA DESA - @yield('title')</title>
+    <title>SI-HUKUM - @yield('title')</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
