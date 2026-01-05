47cd64a20feb3a3fd27df45b4373c4a558485995
falahal23 <falahal24si@mahasiswa.pcr.ac.id>
2026-01-05 09:24:18 +0700

    login email change

--- resources/views/admin/pages/auth/login.blade.php
+++ resources/views/admin/pages/auth/login.blade.php
@@ -106,7 +106,7 @@
 
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                    <input type="text" class="form-control @error('email') is-invalid @enderror" id="email"
                         name="email" value="{{ old('email') }}" placeholder="Masukkan email" required>
                     @error('email')
                         <div class="invalid-feedback">{{ $message }}</div>
